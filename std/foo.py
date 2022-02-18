d43e130a6e43d3961c421a41c59221a0e0a20ff1
AverageAssemblyProgrammer <AverageAssembly8664@gmail.com>
2022-02-18 22:19:46 +0530

    starting to understand rule 110

--- std/foo.py
+++ std/foo.py
@@ -1,3 +1,43 @@
-give_input = float(int(input()))
-print(give_input)
+#!/usr/bin/env python3
 
+# rule110.py
+
+# fully stolen from:- https://github.com/Luki248/rule110
+# this was stolen just to understand how rule110 works, after understanding
+# I will remove this and implement it in hustle
+
+NUMBER_OF_COLUMNS = 120
+
+
+def rule110(number_of_columns):
+    number_of_rows = number_of_columns - 2
+
+    line = " "*(number_of_columns - 1) + "*"
+
+    for i in range(number_of_rows):
+        next_line = " "
+        for j in range(1, number_of_columns-1):
+            pattern = line[j-1] + line[j] + line[j+1]
+            if pattern == "   ":
+                next_line += " "
+            elif pattern == "  *":
+                next_line += "*"
+            elif pattern == " * ":
+                next_line += "*"
+            elif pattern == " **":
+                next_line += "*"
+            elif pattern == "*  ":
+                next_line += " "
+            elif pattern == "* *":
+                next_line += "*"
+            elif pattern == "** ":
+                next_line += "*"
+            elif pattern == "***":
+                next_line += " "
+        next_line += " "
+        line = next_line
+        print(line)
+
+
+if __name__ == "__main__":
+    rule110(NUMBER_OF_COLUMNS)
